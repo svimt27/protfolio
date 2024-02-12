cb1262da5a11c25152aaf9175e1a6598dee38bbd
Shivam Kashyap <shivam.kashyap@thrymr.net>
2024-02-12 15:23:30 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `protfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
